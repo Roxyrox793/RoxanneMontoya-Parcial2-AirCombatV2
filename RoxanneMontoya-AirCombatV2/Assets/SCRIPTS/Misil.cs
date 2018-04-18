a6f0dc6271148662491e25bcfcf66f4c448a36b9
Roxanne <roxyrox793@gmail.com>
2018-04-17 19:26:37 -0500

    V2

---
+++ RoxanneMontoya-AirCombatV2/Assets/SCRIPTS/Misil.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnCollisionEnter(Collision collision){
+		Destroy (collision.collider.gameObject);
+		Destroy (this.gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0f, 0f, 0.7f);  
+		
+	}
+}
